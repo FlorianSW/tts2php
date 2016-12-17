11be8cfc9cde230e1a3bd381d3cb98ea71bbef86
Florian Schmidt <florian.schmidt.stargatewissen@gmail.com>
2016-12-17 18:43:41 +0100

    Fix indention with tabs in config and change default log level
    DEBUG isn't a debug level, which should be used by default, lowering
    to WARNING.

--- config-default.php
+++ config-default.php
@@ -3,7 +3,7 @@
 /**
  * The default log level will be used for each new logger.
  */
-define( 'DEFAULT_LOG_LEVEL', \Monolog\Logger::DEBUG );
+define( 'DEFAULT_LOG_LEVEL', \Monolog\Logger::WARNING );
 /**
  * The log file is the file to which the log output should be streamed, to. The first %s will be
  * replaced by the channel of the logger.
@@ -14,18 +14,18 @@ return [
 	'defaultLanguage' => 'de-DE',
 	'needsEncoding' => true,
 	'fileCacheDir' => dirname( __DIR__ ) . '/cache/',
-    'ttsService' => [
-    	'class' => 'TTS2PHP\\Service\\TTS\\Pico2WaveService',
-        'serviceBinary' => '/usr/bin/pico2wave',
-    ],
-    'encoderService' => [
+	'ttsService' => [
+		'class' => 'TTS2PHP\\Service\\TTS\\Pico2WaveService',
+		'serviceBinary' => '/usr/bin/pico2wave',
+	],
+	'encoderService' => [
 		'class' => 'TTS2PHP\\Service\\Encoder\\SoXEncoderService',
-        'serviceBinary' => '/usr/bin/sox',
-        'channels' => '2',
-        'rate' => '44100',
-    ],
-    'objectCache' => [
-    	'class' => 'TTS2PHP\\Cache\\RedisCache',
-        'arguments' => [],
-    ],
+		'serviceBinary' => '/usr/bin/sox',
+		'channels' => '2',
+		'rate' => '44100',
+	],
+	'objectCache' => [
+		'class' => 'TTS2PHP\\Cache\\RedisCache',
+		'arguments' => [],
+	],
 ];
